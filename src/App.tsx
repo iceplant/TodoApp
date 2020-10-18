3826fb29bbe43924b760243e440b3b950d7d4acb
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2020-10-18 16:08:17 -0700

    working save, delete, title buttons without routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
+import TodoListApp from './TodoListApp'
+//import './styles/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoListApp />
   );
 }
 
 export default App;
+
+
+
+// const rootElement = document.getElementById('root')
+// render(<TodoListApp />, rootElement)
